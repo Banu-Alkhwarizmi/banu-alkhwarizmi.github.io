cf56bb445dbba897f43ee52a348f242d2dcc03e0
sambodjango <gringolalia@gmail.com>
2021-01-20 16:17:21 -0300

    deploy

---
+++ precache-manifest.b00a4dba094ccfdb9b769b5882c5fe4e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4e33c08e2a9ecf3937",
+    "url": "/css/about.6af76146.css"
+  },
+  {
+    "revision": "df474ea2d8cd12ee618e",
+    "url": "/css/app.93143dfd.css"
+  },
+  {
+    "revision": "fe166ba4d10b81d1637e",
+    "url": "/css/chunk-vendors.6b88b602.css"
+  },
+  {
+    "revision": "39083cdef834fe0d7ee39e0272d1aa76",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.39083cde.woff"
+  },
+  {
+    "revision": "d960b795b343e35150c1a0aa3c38c16b",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.d960b795.woff2"
+  },
+  {
+    "revision": "8c4120b4e88276badbc808e518572fad",
+    "url": "/img/logo.8c4120b4.png"
+  },
+  {
+    "revision": "d221a8beea4038e2cfe64c061ed7a630",
+    "url": "/img/nyc-el.d221a8be.jpg"
+  },
+  {
+    "revision": "b25e7784b2594703a5a415663af7748b",
+    "url": "/img/nyc-grand-and-centered.b25e7784.jpg"
+  },
+  {
+    "revision": "7f4a2f25424962ae4244fdc68a120049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b4e33c08e2a9ecf3937",
+    "url": "/js/about.b5fceb1a.js"
+  },
+  {
+    "revision": "df474ea2d8cd12ee618e",
+    "url": "/js/app.f1a5a4a8.js"
+  },
+  {
+    "revision": "fe166ba4d10b81d1637e",
+    "url": "/js/chunk-vendors.366710fd.js"
+  },
+  {
+    "revision": "267dc9d2138e222a86ab6c65a6145d73",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
